7a827923dbcf2b81149c09ffe80d60e79d02948d
Pixelstorm <thepixelstormer@gmail.com>
2024-01-16 16:06:28 +0000

    Move SecurityPermissionAttribute to plugin class file

--- YourModName.cs
+++ YourModName.cs
@@ -1,4 +1,9 @@
-﻿using BepInEx;
+﻿using System.Security.Permissions;
+using BepInEx;
+
+#pragma warning disable CS0618 // SecurityAction.RequestMinimum is obsolete. However, this does not apply to the mod, which still needs it. Suppress the warning indicating that it is obsolete.
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
 
 namespace YourModName
 {
